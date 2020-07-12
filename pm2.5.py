04783e27d3e37f0219c414c1e611e8627dd4b794
Wrg1t <Wrg1t@users.noreply.github.com>
2020-07-12 15:49:44 +0800

    master

---
+++ pm2.5.py
@@ -0,0 +1,28 @@
+import requests
+import traceback
+from xpinyin import Pinyin
+from bs4 import BeautifulSoup
+
+def getHtml(url):
+    try:
+        r = requests.get(url)
+        r.encoding = 'utf-8'
+        r.raise_for_status
+        return r.text
+    except:
+        traceback.print_exc()
+        return ''
+
+def cityDetail(url):
+    HTML = getHtml(url)
+    soup = BeautifulSoup(HTML,'html.parser')
+    digit = soup.select('.aqivalue')
+    return digit[0].text
+
+def main():
+    cityZH = input('要查询的城市名称：')
+    p = Pinyin()
+    cityPY = p.get_pinyin(cityZH,'')
+    url = f'http://www.pm25x.com/city/{cityPY}.htm'
+    print(cityZH,'PM2.5:',cityDetail(url))
+main()
